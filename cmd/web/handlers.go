fbf674ff9f607a7ac62ed84ced8cb04c10e1b989
dl541 <davelei97mathsg@gmail.com>
2024-03-05 19:38:59 -0800

    Finished chapter 5.1

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,9 +3,10 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
+
+	"html/template"
 
 	models "snippetbox.dl541.net/internal/models/errors"
 )
@@ -16,23 +17,33 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"ui/html/base.html",
-		"ui/html/pages/home.html",
-		"ui/html/partials/nav.html",
-	}
-	// w.Write([]byte("Hello from Snippetbox"))
-	ts, err := template.ParseFiles(files...)
-
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, err)
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
+
+	// files := []string{
+	// 	"ui/html/base.html",
+	// 	"ui/html/pages/home.html",
+	// 	"ui/html/partials/nav.html",
+	// }
+
+	// ts, err := template.ParseFiles(files...)
+
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	// err = ts.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// }
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -53,7 +64,26 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%+v", snippet)
+	files := []string{
+		"ui/html/base.html",
+		"ui/html/partials/nav.html",
+		"ui/html/pages/view.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	data := &templateData{
+		Snippet: snippet,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
